7c79201eb359f04b817587f0fb2a7343e8ee26f5
sxr133 <96782879+sxr133@users.noreply.github.com>
2024-03-16 13:06:52 -0400

    adding background image, re-arrange my functions in order, added the disabled/enabled option

--- WordGuess/app.js
+++ WordGuess/app.js
@@ -1,24 +1,27 @@
 // Variables
 const alpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+const capitals = ['Berlin', 'London', 'Paris', 'Rome', 'Ottawa'];
+const teams = ['Montreal Canadiens', 'Toronto BlueJays', 'Boston Red-Sox', 'Los Angeles Dodgers', 'New-York Yankees'];
+const movies = ['Titanic', 'Avatar', 'Rocky', 'Shreik', 'Mission Impossible'];
+
 const options = ['Select Category', 'Sports Team', 'World Capitals', 'Movies'];
 const letters_container = document.querySelector('.letters-container');
 const category_section = document.getElementById('categories');
 const spinner_section = document.getElementById('spinner');
 const word_guessed = document.querySelector('.word-container')
-const capitals = ['Berlin', 'London', 'Paris', 'Washington DC', 'Ottawa'];
-const teams = ['Canadiens', 'Blue Jays', 'Red Sox', 'Dodgers', 'Yankees'];
-const movies = ['Titanic', 'Avatar', 'Rocky', 'Shreik', 'Mission Impossible'];
 const drop_down_label = document.createElement('label')
 const select = document.createElement('select')
-const letter_id = document.getElementById('letter-id')
+const spin_button = document.getElementById('spin_button')
 
 var values = ['100', '200', '300', '400', '500', '1000', '2500', '5000', 'Lose Your Turn', 'Bankrupt', '5000'];
 
+let lives = 5;
 let width = 26;
-let letter_button_id
-let word
+let letter_button_id;
+let word;
 
-// function to create the category drop-down box
+// Top Section
+// Function to create the category drop-down box
 function create_Category_Display() {
   // Label
   drop_down_label.setAttribute('for', 'categories')
@@ -40,28 +43,16 @@ function create_Category_Display() {
   category_section.appendChild(select)
 }
 
-// Function to create the container that holds the letters of the alphabet
-// Buttons are being used for display
-function create_Letter_Display() {
-  for (let i = 0; i < alpha.length ; i++){
-    const letter_button = document.createElement('button')
-    letter_button.setAttribute('button-id', i)
-    letter_button.innerHTML = alpha[i]
-    letters_container.appendChild(letter_button)
-  }
-}
-
-function create_Word_Display() {
-  for (let i = 0; i < 30; i++){
-    const letter_box = document.createElement('div')
-    letter_box.setAttribute('id', 'div' + i)
-    word_guessed.appendChild(letter_box)
-  }
-}
-
 // Function to get the selection chosen by the user
 function changeFunc() {
   var selectBox = document.getElementById("category").value;
+  lives -= 1
+  spin_button.removeAttribute('disabled')
+  console.log(letters_container)
+  for (let i = 0; i < alpha.length ; i++){
+    let btn = document.getElementById('btn' + i);
+    btn.removeAttribute('disabled');
+  }
   getWord(selectBox)
 }
 
@@ -96,10 +87,35 @@ function getWord(selectBox) {
       abc(word);
       break
   }
+  var boxSelected = document.getElementById("category")
+  boxSelected.setAttribute('disabled', true)
+}
+
+// Middle Section
+// Function to create the container that holds the letters of the alphabet
+// Buttons are being used for display
+function create_Letter_Display() {
+  for (let i = 0; i < alpha.length ; i++){
+    const letter_button = document.createElement('button')
+    letter_button.setAttribute('disabled', true)
+    letter_button.setAttribute('id', 'btn' + i)
+    letter_button.innerHTML = alpha[i]
+    letters_container.appendChild(letter_button)
+  }
+}
+
+// Bottom Section
+function create_Word_Display() {
+
+  for (let i = 0; i < 45; i++){
+    const letter_box = document.createElement('div')
+    letter_box.setAttribute('id', 'div' + i)
+    word_guessed.appendChild(letter_box)
+  }
 }
 
 function abc(word) {
-  if (word.length <= 30) {
+  if (word.length <= 45) {
       // Iterate through each character of the word
       for (var i = 0; i < word.length; i++) {
           var char = word.charAt(i);
@@ -123,6 +139,8 @@ function clearcontent(word) {
   } 
 }*/
 
-create_Category_Display();
+create_Category_Display();  
 create_Letter_Display();
 create_Word_Display();
+
+
